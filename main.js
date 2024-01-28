00ff94e7cdf46933b0d84de20c92c400bcd0de75
rana zaid <itszaidrajpoot@gmail.com>
2024-01-28 23:06:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,268 @@
+//  QNo1= Write a logic to print the Fibonacci serious ?
+
+// let num=0;
+// let num1=1;
+// for(let i=0;i<=10;i++){
+//     let fb = num+num1;
+//     num=num1;
+//     num1=fb;
+//     console.log(num1);
+// }
+
+
+
+
+
+//QNo2= how you reverse the  srtring ?
+
+
+// let string="zaid"
+//  for(let i=string.length;i>=0;i--){
+//        console.log(string[i])
+//  }
+
+
+
+
+
+
+//QNo3= Write a logic to find the factorial of a given a number ?
+
+
+// let number=Number(prompt("enter the number"))
+
+// if(number<0){
+//     console.log("factorial of negative number does not exist")
+// }
+// else if(number==0){
+//     console.log("factorial of 0 is 1")
+// }
+//  else{
+     
+//      let me = 1;
+//     for (i = 1; i <= number; i++) {
+//         me *= i;
+//     }
+//     console.log(`The factorial of ${number} is ${me}.`);
+//  }
+
+
+ 
+
+
+
+//QNo4= Find  the occurrences of the given character. ?
+
+
+// let str="classic"
+// let st="a"
+// console.log(`First occurrence of ${st} in  given string is at index: ${str.indexOf(st)}`);
+
+
+
+
+//QNo5= Can you print the first repeating character from the string ?
+
+// let str="zakiz"
+// for(let i=0;i<str.length;i++){
+//     for( let j=i+1;j<str.length;j++){
+//     if(str[i]==str[j]){
+//         console.log(str[i])
+//     }
+//     }
+// }
+
+
+
+//QNo6= How would you check if the given number is integer or not? 
+
+// let number=Number(prompt("enter the number"))
+// if(number%1==0){
+//     console.log("the number is integer")
+    
+// }
+//     else{
+//         console.log("not integers")
+//     }
+
+// Qno7=write a programme to print the sum of first 10 even numbers ?.
+
+
+
+
+
+
+
+
+
+// Qno8=write a programme to print the pyramid of staric using for loops ?
+
+
+
+
+
+
+
+
+1 // Write a js program to find length of a string.
+  
+
+
+
+ // let num ="classic"
+
+ // console.log(num.length)
+
+
+
+
+
+
+2 // Write a js program to concatenate two strings
+
+
+
+// let str1="rana";
+// let str2="Ahmad";
+
+// let res=str1.concat(str2);
+
+// console.log(res)
+
+
+
+3 // Write a js program to convert lowercase string to uppercase.
+
+
+
+// let str1="marasi"
+// console.log(str1.toUpperCase())
+
+
+
+
+4 // Write a js program to convert uppercase string to lowercase.
+
+
+// let str1="MARASI"
+// console.log(str1.toLowerCase());
+
+
+5 // Write a js program to count total number of vowels and consonants in a string.
+ 
+
+
+
+// let string = "iamranaahmad";
+// for (let i = 0; i < string.length; i++) {
+//     let j = string[i];
+
+//     if (j === "a" || j === "e" || j === "i" || j === "o" || j === "u") {
+//         console.log("vowel");
+//     } else {
+//         console.log("consonant");
+//     }
+// }
+
+
+6 // Write a js program to count total number of words in a string.
+
+
+// let str="hello words we are here done"
+// let newStr=str.split(" ").length
+// console.log(newStr)
+
+
+7 // Write a js program to copy one string to another string.
+// let str1="we are "
+// let str2="here"
+// let Str =str2.replace(str1)
+// console.log(Str)
+
+8 // Write a js program to check whether a string is palindrome or not.
+// let str="zaid"
+// let palindrome=false
+// for( let i=0;i<str.length;i++){
+//     for(let j=str.length-1;j>=0;j--){
+//     if(str[i]==str[j]){
+//         palindrome=true
+      
+//       break;  
+//     }
+//     i++
+//     }
+// }
+// if(palindrome){
+//     console.log("palindrome")
+// }
+// else{
+//     console.log("not a palindrome")
+// }
+
+
+// 9 Programme to add first natural numbers ?
+
+// let sum=0
+// let n= prompt("enter thr number")
+// n= Number.parseInt(n)
+// for(let i=1;i<n;i++){
+//     sum +=i
+// }
+// console.log("sum of first " + n + "  natural numbers is "+sum);
+
+
+
+// 10. Write a js program to find first occurrence of a word in a given string.
+
+// 11. Write a js program to find last occurrence of a word in a given string.
+
+// 12. Write a js program to search all occurrences of a word in given string.
+
+// 13. Write a js program to count occurrences of a word in a given string.
+
+// 14. Write a js program to remove first occurrence of a word from string.
+
+// 15. Write a js program to remove last occurrence of a word in given string.
+
+// 16. Write a js program to remove all occurrence of a word in given string.
+
+// 17. Write a js program to trim leading white space characters from given string.
+
+// 18. Write a js program to trim trailing white space characters from given string.
+
+// 19. Write a js program to trim both leading and trailing white space characters from given string.
+
+// 20. Write a js program to remove all extra blank spaces from given string.
+
+// 21. Write a js program to count occurrences of a character in given string.
+
+// 22. Write a js program to find highest frequency character in a string.
+
+// 23. Write a js program to find lowest frequency character in a string.
+
+// 24. Write a js program to count frequency of each character in a string.
+
+// 25. Write a js program to remove first occurrence of a character from string.
+
+// 26. Write a js program to remove last occurrence of a character from string.
+
+// 27. Write a js program to remove all occurrences of a character from string.
+
+// 28. Write a js program to remove all repeated characters from a given string.
+
+// 29. Write a js program to replace first occurrence of a character with another in a string.
+
+// 30. Write a js program to replace last occurrence of a character with another in a string.
+
+// 31. Write a js program to replace all occurrences of a character with another in a string.
+
+
+
+
+
+
+ 
+
+
+
+
